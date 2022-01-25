a796efa352fdf6922261d41ddebb39a64816fe31
dferes <dferes23@gmail.com>
2022-01-24 22:54:26 -0800

    added user parameter to get_user endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import status, HTTPException, Depends, APIRouter
 from schemas.users import UserCreate, UserResponse
-from .. import models, utils
+from .. import models, utils, oauth2
 from ..database import get_db
 from sqlalchemy.orm import Session
 
@@ -29,7 +29,9 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get('/{username}', response_model=UserResponse)
-def get_user(username: str, db: Session = Depends(get_db)):
+def get_user(username: str, db: Session = Depends(get_db), user_: str = Depends(oauth2.get_current_user)):
+    # decide on using the code below or the variable from the Depends(oauth2) above
+    # for getting the user data. Redundant as it is now.
     user = db.query(models.User).filter(models.User.username == username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
