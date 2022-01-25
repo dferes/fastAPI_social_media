c05b889d21917f4502c62af7610131c536d121ac
dferes <dferes23@gmail.com>
2022-01-24 22:42:54 -0800

    db url environment variable now usee

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings 
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql:///fastAPI_1'
+SQLALCHEMY_DATABASE_URL = f"postgresql:///{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, bind=engine) # auto_flush=False ?? Look this up again
