c9d80ae7a8d8aa8261965ee1af00da81f6f5cb57
dferes <dferes23@gmail.com>
2022-01-24 22:46:20 -0800

    Using CORS middleware and disabled sqlalchemy create engine, since alembic is being used now

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine, get_db
-from .routers import posts, users, auth
+from .routers import posts, users, auth, votes
+from fastapi.middleware.cors import CORSMiddleware
 
+# This line is no longer needed, since we're using alembic
+# models.Base.metadata.create_all(bind=engine)
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["*"] # configure this once you build out the client-side web app; make it an env variable
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(votes.router)
 
 
 @app.get('/')
