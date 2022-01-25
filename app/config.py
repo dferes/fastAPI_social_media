d643a4944b81819a18423ff2c0c424e50b4e96ef
dferes <dferes23@gmail.com>
2022-01-24 23:01:02 -0800

    gitignore

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    database_hostname: str 
+    database_port: str = None
+    database_password: str = None
+    database_username: str
+    database_name: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
+
+
+# STOPPED AT 9:17:15
+# need to add/modify a few things in the .gitignore file
