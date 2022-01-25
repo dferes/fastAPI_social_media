be45588e18f7220dcfa0e7a73776f670e9f4c119
dferes <dferes23@gmail.com>
2022-01-24 22:47:58 -0800

    Added foreign key and user relationship to Post table. Created Vote table

--- app/models.py
+++ app/models.py
@@ -1,18 +1,23 @@
-from sqlalchemy.sql.expression import null, text
+from sqlalchemy.sql.expression import text
+from sqlalchemy import ForeignKey
+# from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP, Boolean, String
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 
 
 class Post(Base):
     __tablename__ = 'posts'
     
     id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, ForeignKey('users.username', ondelete='cascade'), nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published= Column(Boolean, default=True, nullable=True)
-    rating = Column(Integer, default = 0, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
+    owner = relationship('User') # This relationship will automatically include all user data associated with the username
     
     
 class User(Base):
@@ -23,3 +28,10 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
+
+class Vote(Base):
+    __tablename__ = 'votes'
+    
+    username = Column(String, ForeignKey('users.username', ondelete='cascade'), primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='cascade'), primary_key=True)    
+    
