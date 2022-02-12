7705dd7829a10dca3adc0a0c1fb288fe19c5cfdc
dferes <dferes23@gmail.com>
2022-02-12 09:43:21 -0800

    Removed the credentials exception to a more global scope, outside of the get_current_user function. Used as default exception

--- app/oauth2.py
+++ app/oauth2.py
@@ -17,6 +17,11 @@ ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE = settings.access_token_expire_minutes
 
 
+credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail='Could not validate credentials.', 
+                                          headers={'WWW-Authenticate': 'Bearer'} )
+
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE)
@@ -27,10 +32,10 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def verify_access_token(token: str, credentials_exception):
+def verify_access_token(token: str, credentials_exception=credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # this line is throwing error...wtf
-    
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
         username: str = payload.get('username')
 
         if username is None: raise credentials_exception
@@ -44,25 +49,8 @@ def verify_access_token(token: str, credentials_exception):
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     '''Ensures that a user is logged in, not that an AUTHORIZED user is logged in''' 
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail='Could not validate credentials.', 
-                                          headers={'WWW-Authenticate': 'Bearer'} )
-    
     token = verify_access_token(token, credentials_exception)
     # throw error here if no user found ..
     user = db.query(models.User).filter(models.User.username == token.username).first()
     
     return user
-
-
-
-# Probably best to use middleware for the code below
-
-# def ensure_authorized_user(username: str, token: str = Depends(oauth2_scheme)):
-#     token_data = get_current_user(token)
-    
-#     if token_data.username != username:
-#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-#                                           detail='Forbidden.', 
-#                                           headers={'WWW-Authenticate': 'Bearer'} )
-#     return True
